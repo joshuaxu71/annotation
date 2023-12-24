27905205d9693de1d116a4ba53345dcf230d1aba
joshuaxu71 <joshuaxu71@gmail.com>
2023-12-24 10:44:01 +0700

    Create the annotation processor and update Main to refer to a non-existent file

--- manual-generate/src/main/java/Main.java
+++ manual-generate/src/main/java/Main.java
@@ -1,9 +1,13 @@
+//import generated.AnnotatedStudent;
+
 public class Main {
 	public static void main(String[] args) {
 		Student student = new Student("Joshua", "joshuaxu71@gmail.com");
 		AnnotatedStudent annotatedStudent = new AnnotatedStudent("Joshua", "joshuaxu71@gmail.com");
+		generated.AnnotatedStudent generatedAnnotatedStudent = new generated.AnnotatedStudent("Joshua (Generated)", "joshuaxu71@gmail.com");
 
 		System.out.println(student);
 		System.out.println(annotatedStudent);
+		System.out.println(generatedAnnotatedStudent);
 	}
 }
