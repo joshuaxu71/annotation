06e6db9ec25f4136084e0b8c688aec268cad0e44
Joshua <49599589+joshuaxu71@users.noreply.github.com>
2023-12-25 10:17:23 +0700

    add auto-service and auto-service-impl module (#5)
    * Add auto-service module
    
    * Update README.md with links

---
+++ auto-service-impl/README.md
@@ -0,0 +1,5 @@
+# Implementation with auto-service
+
+This is just a duplicate of the manual-replace module, but the registration uses @AutoService in 
+the [auto-service](https://github.com/joshuaxu71/annotation/tree/auto-service/auto-service) module. 
+You can check the description there for more information.
---
+++ auto-service/src/main/java/CustomToStringProcessor.java
@@ -0,0 +1,52 @@
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.google.auto.service.AutoService;
+
+import javax.annotation.processing.*;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Set;
+
+@AutoService(Processor.class)
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({"CustomToString"})
+public class CustomToStringProcessor extends AbstractProcessor {
+
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+		for (TypeElement annotation : annotations) {
+			Set<? extends Element> annotatedElements = roundEnvironment.getElementsAnnotatedWith(annotation);
+
+			for (Element element : annotatedElements) {
+				try {
+					addToStringMethodToClass((TypeElement) element, processingEnv);
+				} catch (IOException e) {
+					processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+				}
+			}
+		}
+		return true;
+	}
+
+	private void addToStringMethodToClass(
+		TypeElement typeElement,
+		ProcessingEnvironment processingEnv
+	) throws IOException {
+		String filePath = CustomToStringProcessorHelper.getFilePath(typeElement, processingEnv);
+		CompilationUnit cu = CustomToStringProcessorHelper.getCompilationUnit(filePath);
+
+		ClassOrInterfaceDeclaration declaration = CustomToStringProcessorHelper.addToStringMethod(typeElement, cu);
+		if (declaration == null) {
+			return;
+		}
+
+		// Write the modified content back to the file
+		try (FileOutputStream out = new FileOutputStream(filePath)) {
+			out.write(cu.toString().getBytes());
+		}
+	}
+}
